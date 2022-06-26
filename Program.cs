a2846ebcbde65a09775eea207b45a600136d6b2d
MuhammadA-C <phantomganhdi@gmail.com>
2022-06-25 23:04:47 -0400

    First commit
    Testing GitHub setup!

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Calculator_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
