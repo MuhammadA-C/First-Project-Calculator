12f18634a6abc6545757053361adc58c456b24fd
MuhammadA-C <phantomganhdi@gmail.com>
2022-06-28 21:32:27 -0400

    First Commit
    Creating basic functions for the calculator console program

--- Program.cs
+++ Program.cs
@@ -4,11 +4,38 @@ namespace Calculator_Project
 {
     class Program
     {
+
+        //Note: Going to use decimal as the data type
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Finally setup Github in Visual Studio!");
+           
+        }
+
+        decimal Addition(decimal number1, decimal number2)
+        {
+            decimal result = number1 + number1;
+            return result;
+        }
+
+        decimal Subtraction(decimal number1, decimal number2)
+        {
+            decimal result = number1 - number2;
+            return result;
+        }
+
+        decimal Multiplicaiton(decimal number1, decimal number2)
+        {
+            decimal result = number1 * number2;
+            return result;
+        }
+
+        decimal Division(decimal number1, decimal number2)
+        {
+            decimal result = number1 / number2;
+            return result;
         }
     }
+    
 }
 
